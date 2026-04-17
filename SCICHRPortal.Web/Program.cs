74ee0c90a06ea673d966447e71d7b4e1a70add62
jayseverino-scic <jayseverino.scic@gmail.com>
2026-04-17 15:31:09 +0800

    resolved login issue

--- SCICHRPortal.Web/Program.cs
+++ SCICHRPortal.Web/Program.cs
@@ -1,7 +1,87 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapStaticAssets();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}")
+//    .WithStaticAssets();
+
+
+//app.Run();
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SCICHRPortal.Utility.Cryptography.Models;
+using System.Net;
+using System.Text;
+using SCICHRPortal.Utility.Cryptography.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.ConfigureAppConfiguration((context, config) =>
+{
+    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+});
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+var jwtSecretKeySection = builder.Configuration.GetSection("JWTSecretKey");
+builder.Services.Configure<JWTSecretKey>(jwtSecretKeySection);
+
+var jwtSecret = jwtSecretKeySection.Get<JWTSecretKey>();
+var key = Encoding.ASCII.GetBytes(jwtSecret.Key!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+    options.ForwardSignIn = "/Login";
+});
+
+builder.Services.AddHttpContextAccessor();
+
+
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -13,17 +93,44 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (context, next) =>
+{
+    var authHeader = context.Request.Cookies.FirstOrDefault(c => c.Key == "jsonWebToken");
+
+    if (authHeader.Value != null)
+        context.Request.Headers.Add("Authorization", "bearer " + authHeader.Value);
+
+    await next.Invoke();
+});
+
+//app.MapControllers();
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
 app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseStatusCodePages(context =>
+{
+    var request = context.HttpContext.Request;
+    var response = context.HttpContext.Response;
+
+    if (response.StatusCode == (int)HttpStatusCode.Unauthorized)
+        response.Redirect("/Login/Index");
+
+    return Task.CompletedTask;
+});
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Login}/{action=Index}/{id?}");
+});
 
 app.Run();
